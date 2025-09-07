04b5248ab0586aaa75fd33bfa8a42da7bc2b8d38
yyj0522 <aha2408@naver.com>
2025-09-08 00:40:12 +0900

    웹앱 자동빌드 테스트

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
     }}>
       <h1 style={{ fontSize: '64px', margin: 0 }}>CampusEat!</h1>
       <p style={{ fontSize: '20px', marginTop: '20px', lineHeight: '1.5', maxWidth: '600px' }}>
-        대학생을 위한 종합 캠퍼스 플랫폼~!<br />
+        대학생을 위한 종합 캠퍼스 플랫폼!<br />
         (맛집 추천, 번개 모임, 학식/셔틀, 커뮤니티)
       </p>
     </div>
