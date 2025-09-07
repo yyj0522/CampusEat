6369152986d826795f60943bef2b921fb20b6d43
yyj0522 <aha2408@naver.com>
2025-09-08 00:34:09 +0900

    웹앱 자동빌드 테스트

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,8 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
+const nextConfig = {
+  output: "export",
 };
 
 export default nextConfig;
