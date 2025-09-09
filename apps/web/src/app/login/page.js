12e6fb4264bf5ede7dedfa4b82d65ba524f96fa0
yyj0522 <aha2408@naver.com>
2025-09-10 03:05:46 +0900

    웹 빌드 테스트

--- apps/web/src/app/login/page.js
+++ apps/web/src/app/login/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import styles from "./LoginPage.module.css";
+import styles from "./LoginPage.css";
 
 export default function LoginPage() {
   return (
